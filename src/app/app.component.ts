b1b523d343e4f4953f3accaa2bd831d53d3e938f
JoshSevy <joshuasevy@outlook.com>
2021-01-14 19:59:55 -0700

    Add date pipe and format to match app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name: string = '';
-  date: string = '';
+  paymentDate: string = '';
 
   onNameChange(value: string): void {
     this.name = value;
   }
 
   onDateChange(value: string): void {
-    this.date = value;
+    this.paymentDate = value;
   }
 }
